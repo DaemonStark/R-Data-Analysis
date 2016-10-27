b5dd032efb656756e8cf431ba00bbf2c04885e63
Rashnil2016 <rashnil@live.unc.edu>
2016-10-27 01:13:06 -0400

    Cross Validation -Logistic Regression of Churn Data

--- R/Churn/data_cleaning.R
+++ R/Churn/data_cleaning.R
@@ -1,4 +1,22 @@
 cleanChurnData <- function()
 {
+churn_data <- read.csv("data/churn.csv",header = T,sep = ",");
+
+churn_data_frame<- as.data.frame(churn_data);
+
+train <- createDataPartition(churn_data_frame$LEAVE,p=0.60,list = FALSE)
+
+train_data <- churn_data_frame[ train, ]
+test_data <- churn_data_frame[ -train, ]
+
+ctrl <-trainControl(method = "repeatedcv",number = 10,savePredictions = TRUE)
+
+model<-train(LEAVE~INCOME+HOUSE+HANDSET_PRICE+COLLEGE,data = train_data,method="glm",family = "binomial",trControl=ctrl,tuneLength=5)
+
+pred <- predict(model,newdata = test_data)
+
+matrixValidation <- confusionMatrix(data=pred,test_data$LEAVE)
+
+print(matrixValidation);
 
 }
