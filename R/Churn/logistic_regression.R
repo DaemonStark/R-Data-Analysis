119a649f3e5840aaffceb00cd9d7041bc6268d37
Rashnil2016 <rashnil@live.unc.edu>
2016-10-29 10:52:40 -0400

    air pollution analysis pm2.5

--- R/Churn/logistic_regression.R
+++ R/Churn/logistic_regression.R
@@ -1,9 +1,13 @@
+#FUnction to perform logistic regression using churn call data to predict whether the customer will leave or stay
+# Cross validation of 10 folds is used for this model
 logistic_regression_cv <- function()
 {
+#read data from csv file
 churn_data <- read.csv("data/churn.csv",header = T,sep = ",");
 
 churn_data_frame<- as.data.frame(churn_data);
 
+#create a training model with 60% of the original data.
 train <- createDataPartition(churn_data_frame$LEAVE,p=0.60,list = FALSE)
 
 train_data <- churn_data_frame[ train, ]
---
+++ R/airpollution/air_pollution_analysis.R
@@ -0,0 +1,6 @@
+load_air_pol_data <- function()
+{
+  air_data <- read.csv("data/daily_88101_2016.csv",header = T,sep = ",");
+
+
+}
