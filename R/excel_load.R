6f02057e4581ac5bc776b00a1661fde87347dad3
Rashnil2016 <rashnil@live.unc.edu>
2016-11-15 18:56:01 -0500

    recommender analysis

--- R/excel_load.R
+++ R/excel_load.R
@@ -11,3 +11,14 @@ sum(na.omit(val)>=24);
 xlurl<- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
 download.file(xlurl,destfile = "R/microSurvey/data2.xlxs")
 
+library("xlsx")
+
+titanic <- read.xlsx("R/microSurvey/titanic.xls",sheetIndex = 1)
+names(titanic)
+
+chisq.test(titanic$pclass,titanic$survived)
+
+fisher.test(titanic$pclass,titanic$survived)
+
+mushroom <- read.csv("R/microSurvey/mushroom.csv",header = T)
+names(mushroom)
